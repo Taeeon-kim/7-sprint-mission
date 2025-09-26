ae86078e6361fcc0ac24a8fecaad32b8f8693960
taeeonkim <myhom43@gmail.com>
2025-09-26 22:28:08 +0900

    add Interface Methods of Service Layers - ChannelService, UserService,

--- src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/ChannelService.java
@@ -1,14 +1,56 @@
 package com.sprint.mission.discodeit.service;
 
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.entity.User;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface ChannelService {
+
+    // === 명령 (Command) 영역 ===
     // 채널 생성
-    // 채널 수정
+    public void createChannel(
+            String title,
+            String description,
+            List<User> joinedUsers,
+            List<Message> messages,
+            User createdBy,
+            boolean isPrivate
+    ); // TODO: 진행시 일반 List형식말고 HashMap 으로 key : UUID, value : List<Channel> 이렇게 -> 이건 인메모리 jcf 부분에서 하면됨 여기랑 별도
+
+    // 채널 수정 -> 어차피 DB없으니 넘겨주는 값보고 같은지 비교후 다르면 해당 부분수정(그래야 "" 이런것도 지운걸로 인식할테니)
+    public void updateChannel(
+            UUID channelId,
+            String title,
+            String description,
+            List<User> joinedUsers,
+            List<Message> messages,
+            User createdBy,
+            boolean isPrivate);
+
     // 채널 삭제
+    public void deleteChannel(UUID channelId); // TODO: HashMap이므로 key 조회후 바로삭제 -> 이건 인메모리 jcf 부분에서 하면됨 여기랑 별도
+
     // 채널 읽기(정보보기위해)
+    public Channel getChannel(UUID channelId); // TODO: HashMap이므로 KEY조회 -> 이건 인메모리 jcf 부분에서 하면됨 여기랑 별도
     // 초기 메세지 가져오기 // 어차피 실시간 아니므로 단일 혹은 요청시에 보내는걸로?
+
     // 채널 입장
+    public void joinChannel(UUID channelId, User user);
+
     // 채널 퇴장
-    // 참여인원조회 getMembers
-    // 전체 채널목록 불러오기 getChannels (전체 체널 리스트)
-    // 특정 유저의 채널 목록 불러오기 getChannelsByUser(userId)
+    public void leaveChannel(UUID channelId, User user);
+
+    //=== 조회 (Query) 영역 ===
+
+    // 참여인원조회 getAllMembers
+    public List<User> getAllMembers(UUID channelId);
+
+    // 전체 채널목록 불러오기 getAllChannels (전체 체널 리스트)
+    public List<Channel> getAllChannels();
+
+    // 특정 유저의 채널 목록 불러오기 getUserChannels(userId)
+    public List<Channel> getChannelsByUserId(UUID userId);
 }
--- src/main/java/com/sprint/mission/discodeit/service/UserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -1,10 +1,21 @@
 package com.sprint.mission.discodeit.service;
 
+import com.sprint.mission.discodeit.entity.User;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface UserService {
     // 회원가입
+    public void signUp(String nickname, String email, String password, String phoneNumber);
     // 회원정보 읽기
+    public User getUserById(UUID userId);
     // 회원탈퇴
+    public void deleteUser(UUID userId);
     // 정보수정
-    // 모든 유저리스트 읽기
+    public void updateUser(UUID userId, String nickname, String email, String phoneNumber);
+
+    // 모든 유저리스트 읽기(관리측면 메서드)
+    public List<User> getAllUsers();
 
 }
