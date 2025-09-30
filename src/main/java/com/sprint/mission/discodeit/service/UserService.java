68bc5858515f260539053926ee1a2af07a505b31
taeeonkim <myhom43@gmail.com>
2025-09-30 11:33:24 +0900

    fix: MessageService, UserService 인터페이스도 명시적 파라미터로 변경

--- src/main/java/com/sprint/mission/discodeit/service/UserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -3,18 +3,18 @@ package com.sprint.mission.discodeit.service;
 import com.sprint.mission.discodeit.entity.User;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.UUID;
 
 public interface UserService {
     // 회원가입
-    public void signUp(User user);
+    //  User user = new User("youngble", "email@example.com", "password123", USER, "010-1234-5678");
+    public void signUp(String nickname, String email, String password, String phoneNumber);
     // 회원정보 읽기
     public User getUserById(UUID userId);
     // 회원탈퇴
     public void deleteUser(UUID userId);
     // 정보수정
-    public void updateUser(User user);
+    public void updateUser(UUID userId, String nickname, String email, String password, String phoneNumber);
 
     // 모든 유저리스트 읽기(관리측면 메서드)
     public List<User> getAllUsers();
