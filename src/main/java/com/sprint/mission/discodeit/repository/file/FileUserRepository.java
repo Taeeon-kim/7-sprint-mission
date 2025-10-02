8ed98d7abff0c8f0a34d39bd829592e7a16c4142
taeeonkim <myhom43@gmail.com>
2025-10-02 17:03:44 +0900

    style: 줄제거

--- src/main/java/com/sprint/mission/discodeit/repository/file/FileUserRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileUserRepository.java
@@ -1,5 +1,4 @@
 package com.sprint.mission.discodeit.repository.file;
-
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.store.Store;
