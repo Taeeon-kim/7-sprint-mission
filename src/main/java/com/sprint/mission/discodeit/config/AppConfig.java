32b543d5c8f79b48f149264af4cde0d1c022420c
taeeonkim <myhom43@gmail.com>
2025-10-02 17:03:11 +0900

    feature/fix: appConfig 수정
    - channerRepository 및 channerService 추가 및 수정
    -

--- src/main/java/com/sprint/mission/discodeit/config/AppConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/AppConfig.java
@@ -1,10 +1,13 @@
 package com.sprint.mission.discodeit.config;
 
+import com.sprint.mission.discodeit.repository.ChannelRepository;
 import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.repository.file.FileChannelRepository;
 import com.sprint.mission.discodeit.repository.file.FileUserRepository;
 import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.MessageService;
 import com.sprint.mission.discodeit.service.UserService;
+import com.sprint.mission.discodeit.service.file.FileChannelService;
 import com.sprint.mission.discodeit.service.file.FileUserService;
 import com.sprint.mission.discodeit.service.jcf.JCFChannelService;
 import com.sprint.mission.discodeit.service.jcf.JCFMessageService;
@@ -13,10 +16,10 @@ public class AppConfig {
     // Service Config
     // NOTE: 싱글톤을 유지하기위해 private final로 한번 생성
     private final UserRepository userRepository = new FileUserRepository();
-
+    private final ChannelRepository channelRepository = new FileChannelRepository();
 
     private final UserService userService = new FileUserService(userRepository);
-    private final ChannelService channelService = new JCFChannelService(userService);
+    private final ChannelService channelService = new FileChannelService(userService, channelRepository);
     private final MessageService messageService = new JCFMessageService(userService, channelService);
 
     // User
@@ -31,6 +34,10 @@ public class AppConfig {
     // Channel
     public ChannelService channelService() {
         return channelService;
+    }
+
+    public ChannelRepository channelRepository() {
+        return channelRepository;
     }
 
     // Message
