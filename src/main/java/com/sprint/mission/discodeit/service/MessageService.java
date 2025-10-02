60a50b8bb7459651a050c29e56f09aec7c633539
taeeonkim <myhom43@gmail.com>
2025-10-02 13:42:10 +0900

    fix: Service 인터페이스 불필요 접근제어자 제거

--- src/main/java/com/sprint/mission/discodeit/service/MessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/MessageService.java
@@ -7,25 +7,25 @@ import java.util.UUID;
 
 public interface MessageService {
     // 모든 메세지 읽기(채널상관없이)
-    public List<Message> getAllMessages();
+    List<Message> getAllMessages();
 
     // 특정 채널의 모든 메세지 읽기
-    public List<Message> getAllMessagesOfChannel(UUID channelId);
+    List<Message> getAllMessagesOfChannel(UUID channelId);
 
     // 단일 특정 메세지 읽기
-    public Message getMessageById(UUID messageId);
+    Message getMessageById(UUID messageId);
 
     // 특정 메세지(uuid 사용)생성 후 보내기(채널로)
-    public void sendMessageToChannel(UUID channelId, UUID senderId, String content); // TODO: 추후 컨트롤러 계층생성시 파라미터를 DTO로 변경(파라미터가 길어질시)
+    void sendMessageToChannel(UUID channelId, UUID senderId, String content); // TODO: 추후 컨트롤러 계층생성시 파라미터를 DTO로 변경(파라미터가 길어질시)
     // 메세지 보내기(개인) -> 결국 개인도 하나의 또다른 채널에서 1:1로만 한다고 가정하면될듯, 그럼 receiver가 필요한가? 개인이면 필요, 그룹이면 Null?
 
     // 특정 메세지(uuid 사용) 수정하기
-    public void updateMessage(UUID messageId, String content);
+    void updateMessage(UUID messageId, String content);
 
     // 특정 메세지(uuid 사용) 삭제하기
-    public void deleteMessage(UUID messageId);
+    void deleteMessage(UUID messageId);
 
-//    public List<Message> getAllMessagesByIds(List<UUID> messageIds);
+//     List<Message> getAllMessagesByIds(List<UUID> messageIds);
 
 
 }
