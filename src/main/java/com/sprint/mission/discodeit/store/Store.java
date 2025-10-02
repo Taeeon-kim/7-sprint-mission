7d2b5ab333d8d3baa81326395ab282500c70b995
taeeonkim <myhom43@gmail.com>
2025-10-02 13:46:32 +0900

    add/fix: Store 수정
    - 각 도메인 data 파일위치 상수값 추가
    - @SuppressWarnings추가로 반환타입 강제무시

--- src/main/java/com/sprint/mission/discodeit/store/Store.java
+++ src/main/java/com/sprint/mission/discodeit/store/Store.java
@@ -8,6 +8,11 @@ import java.util.Map;
 
 
 public class Store {
+    public static final Path USER_DATA_DIR = Path.of(System.getProperty("user.dir"), "data");
+    public static final Path USER_DATA_FILE = Path.of(USER_DATA_DIR.toString(), "users.ser");
+    public static final Path CHANNEL_DATA_FILE = Path.of(USER_DATA_DIR.toString(), "channels.ser");
+    public static final Path MESSAGE_DATA_FILE = Path.of(USER_DATA_DIR.toString(), "messages.ser");
+
 
     public static <K extends Serializable, V extends Serializable> void saveMap(Path file, Map<K, V> map) {
         try {
@@ -25,13 +30,17 @@ public class Store {
         }
     }
 
+    @SuppressWarnings("unchecked")
     public static <K extends Serializable, V extends Serializable> Map<K, V> loadMap(Path file) {
         if (!Files.exists(file)) {
             return new HashMap<>();
         }
         try (var ois = new ObjectInputStream(new BufferedInputStream(Files.newInputStream(file)))) {
             Object obj = ois.readObject();
-            return (Map<K, V>) obj;
+            if ((obj instanceof Map<?, ?> map)) {
+                return new HashMap<>((Map<K, V>) map);
+            }
+            throw new IllegalStateException("저장된 데이터가 Map이 아님!");
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         } catch (ClassNotFoundException e) {
