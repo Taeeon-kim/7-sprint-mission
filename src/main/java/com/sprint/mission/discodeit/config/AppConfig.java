268dae25b937b734d78fef2105c714493514798b
taeeonkim <myhom43@gmail.com>
2025-10-02 13:39:42 +0900

    feature: AppConfig에 UserRepository 추가

--- src/main/java/com/sprint/mission/discodeit/config/AppConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/AppConfig.java
@@ -1,8 +1,11 @@
 package com.sprint.mission.discodeit.config;
 
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.repository.file.FileUserRepository;
 import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.MessageService;
 import com.sprint.mission.discodeit.service.UserService;
+import com.sprint.mission.discodeit.service.file.FileUserService;
 import com.sprint.mission.discodeit.service.jcf.JCFChannelService;
 import com.sprint.mission.discodeit.service.jcf.JCFMessageService;
 import com.sprint.mission.discodeit.service.jcf.JCFUserService;
@@ -10,13 +13,20 @@ import com.sprint.mission.discodeit.service.jcf.JCFUserService;
 public class AppConfig {
     // Service Config
     // NOTE: 싱글톤을 유지하기위해 private final로 한번 생성
-    private final UserService userService = new JCFUserService();
+    private final UserRepository userRepository = new FileUserRepository();
+
+
+    private final UserService userService = new FileUserService(userRepository);
     private final ChannelService channelService = new JCFChannelService(userService);
     private final MessageService messageService = new JCFMessageService(userService, channelService);
 
     // User
     public UserService userService() {
         return userService;
+    }
+
+    public UserRepository userRepository() {
+        return userRepository;
     }
 
     // Channel
