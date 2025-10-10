21951b01ec4b9eb0d4655c3dd6a300f3ade8d62f
taeeonkim <myhom43@gmail.com>
2025-10-10 12:03:43 +0900

    feature:  FileMessageService message 핸들링 관련 서비스 구현체 구현
    - MessageService 구현체 오버라이딩.
    - message관리 인터페이스 메서드 구현.
    - 비즈니스로직에 필요한 ChannelService, UserService, and MessageRepository를 통합하여 사용.

--- src/main/java/com/sprint/mission/discodeit/service/file/FileMessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/file/FileMessageService.java
@@ -1,4 +1,88 @@
 package com.sprint.mission.discodeit.service.file;
 
-public class FileMessageService {
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+import com.sprint.mission.discodeit.service.ChannelService;
+import com.sprint.mission.discodeit.service.MessageService;
+import com.sprint.mission.discodeit.service.UserService;
+
+import java.util.*;
+
+public class FileMessageService implements MessageService {
+
+    private final ChannelService channelService;
+    private final UserService userService;
+    private final MessageRepository messageRepository;
+
+    public FileMessageService(UserService userService, ChannelService channelService, MessageRepository messageRepository) {
+        this.userService = userService;
+        this.channelService = channelService;
+        this.messageRepository = messageRepository;
+    }
+
+
+    @Override
+    public List<Message> getAllMessages() {
+        Map<UUID, Message> allMessages = messageRepository.findAllMap();
+        return allMessages.values()
+                .stream()
+                .sorted(Comparator.comparing(Message::getCreatedAt))
+                .toList();
+    }
+
+    @Override
+    public List<Message> getAllMessagesOfChannel(UUID channelId) {
+        if (channelId == null) {
+            throw new IllegalArgumentException("입력값이 잘못 되었습니다.");
+        }
+        Channel channel = channelService.getChannel(channelId);
+        Map<UUID, Message> allMessages = messageRepository.findAllMap();
+        return channel.getMessageIds()
+                .stream()
+                .map(allMessages::get)
+                .filter(Objects::nonNull)
+                .sorted(Comparator.comparing(Message::getCreatedAt))
+                .toList();
+    }
+
+    @Override
+    public Message getMessageById(UUID messageId) {
+        return null;
+    }
+
+    public void sendMessageToChannel(UUID channelId, UUID senderId, String content) { // TODO: 추후 컨트롤러 계층생성시 파라미터를 DTO로 변경(파라미터가 길어질시)
+        if (channelId == null || senderId == null || content == null) { // TODO: 추후 컨트롤러 생성시 책임을 컨트롤러로 넘기고 트레이드오프로 신뢰한다는 가정하에 진행 , 굳이 방어적코드 x
+            throw new IllegalArgumentException("입력값이 잘못 되었습니다.");
+        }
+        // NOTE: 1. 보내려는 유저가 맞는지 확인
+        User sender = userService.getUserById(senderId);
+        // NOTE: 2. 보내려는 채널이있는지 확인
+        Channel channel = channelService.getChannel(channelId);
+        boolean isMember = channel.isMember(sender.getId());
+        if (!isMember) {
+            throw new IllegalStateException("채널 맴버만 메세지 전송 가능합니다.");
+        }
+        Message message = new Message(content, senderId, channelId);
+
+        // NOTE: 3. 메세지를 전역 Message 저장소에 저장
+        messageRepository.save(message);
+        // NOTE: 4. 해당 채널에 messageId 추가
+        channelService.addMessageToChannel(channel, message.getId());
+
+
+//        channel.setUpdatedAt(System.currentTimeMillis()); // NOTE: 보통 메타정보와 메세지 변경을 분리해야하지만 일단 모두 변경으로 인식
+
+    }
+
+    @Override
+    public void updateMessage(UUID messageId, String content) {
+
+    }
+
+    @Override
+    public void deleteMessage(UUID messageId) {
+
+    }
 }
