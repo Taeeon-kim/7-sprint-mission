53fc631f68a1ed2219ad51b3f6c9563a54eb9b42
taeeonkim <myhom43@gmail.com>
2025-10-10 12:05:26 +0900

    feature: MessageRepository 인터페이스 메서드 추가
    - 메세지 저장을 위한 save 메서드 추가.
    - 모든 메세지를 Map타입형식으로 얻기위한 findAllMap 인터페이스메서드추가.

--- src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
@@ -1,4 +1,13 @@
 package com.sprint.mission.discodeit.repository;
 
+import com.sprint.mission.discodeit.entity.Message;
+
+import java.util.Map;
+import java.util.UUID;
+
 public interface MessageRepository {
+
+    void save(Message message);
+
+    Map<UUID, Message> findAllMap();
 }
