61278e9ff23aa5f34212f3c695ccdb517ba3b5ef
taeeonkim <myhom43@gmail.com>
2025-10-13 13:39:43 +0900

    feature: MessageRepository와 FileMessageRepository 에 새 메서드들 추가
    - `deleteById` 와 `findById` 메서드 추가 in MessageRepository interface.
    -  `deleteById` 와 `findById` 메서드 구현 in FileMessageRepository.

--- src/main/java/com/sprint/mission/discodeit/repository/file/FileMessageRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileMessageRepository.java
@@ -4,6 +4,7 @@ import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.repository.MessageRepository;
 import com.sprint.mission.discodeit.store.Store;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 public class FileMessageRepository implements MessageRepository {
@@ -15,8 +16,25 @@ public class FileMessageRepository implements MessageRepository {
     }
 
     @Override
+    public boolean deleteById(UUID id) {
+        Map<UUID, Message> allMessages = findAllMap();
+        Message remove = allMessages.remove(id);
+        if (remove != null) {
+            Store.saveMap(Store.MESSAGE_DATA_FILE, allMessages);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
     public Map<UUID, Message> findAllMap() {
         return Store.loadMap(Store.MESSAGE_DATA_FILE);
+    }
+
+    @Override
+    public Optional<Message> findById(UUID id) {
+        Map<UUID, Message> allMessages = findAllMap();
+        return Optional.ofNullable(allMessages.get(id));
     }
 
 }
