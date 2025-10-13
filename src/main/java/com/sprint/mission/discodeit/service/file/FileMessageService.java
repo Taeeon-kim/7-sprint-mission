329605c97b1ffe875b38a8fe6c84725b13c0dca0
taeeonkim <myhom43@gmail.com>
2025-10-13 13:42:29 +0900

    refactor: FileMessageService 로직 개선 및 ChannelRepository 의존성 추가
    - ChannelRepository를 FileMessageService 생성자와 관련 메서드에 추가.
    - getMessageById, updateMessage, deleteMessage 메서드 로직을 개선하여 입력값 검증 및 예외 처리 추가.
    - 메시지 CRUD 시 Channel entity와의 연동 로직 구현 및 repository 저장 추가.

--- src/main/java/com/sprint/mission/discodeit/service/file/FileMessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/file/FileMessageService.java
@@ -3,6 +3,7 @@ package com.sprint.mission.discodeit.service.file;
 import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
 import com.sprint.mission.discodeit.repository.MessageRepository;
 import com.sprint.mission.discodeit.service.ChannelService;
 import com.sprint.mission.discodeit.service.MessageService;
@@ -15,11 +16,13 @@ public class FileMessageService implements MessageService {
     private final ChannelService channelService;
     private final UserService userService;
     private final MessageRepository messageRepository;
+    private final ChannelRepository channelRepository;
 
-    public FileMessageService(UserService userService, ChannelService channelService, MessageRepository messageRepository) {
+    public FileMessageService(UserService userService, ChannelService channelService, MessageRepository messageRepository, ChannelRepository channelRepository) {
         this.userService = userService;
         this.channelService = channelService;
         this.messageRepository = messageRepository;
+        this.channelRepository = channelRepository;
     }
 
 
@@ -49,7 +52,8 @@ public class FileMessageService implements MessageService {
 
     @Override
     public Message getMessageById(UUID messageId) {
-        return null;
+        if (messageId == null) throw new IllegalArgumentException("입력값이 잘못 되었습니다.");
+        return messageRepository.findById(messageId).orElseThrow(() -> new NoSuchElementException("메세지가 없습니다."));
     }
 
     public void sendMessageToChannel(UUID channelId, UUID senderId, String content) { // TODO: 추후 컨트롤러 계층생성시 파라미터를 DTO로 변경(파라미터가 길어질시)
@@ -68,9 +72,9 @@ public class FileMessageService implements MessageService {
 
         // NOTE: 3. 메세지를 전역 Message 저장소에 저장
         messageRepository.save(message);
-        // NOTE: 4. 해당 채널에 messageId 추가
-        channelService.addMessageToChannel(channel, message.getId());
-
+        // NOTE: 4. 해당 채널에 messageId 추가 및 업데이트
+        channel.addMessageId(message.getId());
+        channelRepository.save(channel);
 
 //        channel.setUpdatedAt(System.currentTimeMillis()); // NOTE: 보통 메타정보와 메세지 변경을 분리해야하지만 일단 모두 변경으로 인식
 
@@ -78,11 +82,34 @@ public class FileMessageService implements MessageService {
 
     @Override
     public void updateMessage(UUID messageId, String content) {
+        if (messageId == null || content == null || content.trim().isEmpty()) {
+            throw new IllegalArgumentException("입력값이 잘못되었습니다.");
+        }
 
+        Message message = getMessageById(messageId);
+        boolean isUpdated = false;
+        if (!content.equals(message.getContent())) {
+            isUpdated = message.updateContent(content);
+        }
+
+        if (isUpdated) {
+            message.setUpdatedAt(System.currentTimeMillis());
+            messageRepository.save(message);
+        }
     }
 
     @Override
     public void deleteMessage(UUID messageId) {
-
+        if (messageId == null) {
+            throw new IllegalArgumentException("전달값이 잘못되었습니다.");
+        }
+        Message message = getMessageById(messageId);
+        boolean isDeleted = messageRepository.deleteById(messageId);
+        if (isDeleted) {
+            Channel channel = channelRepository.findById(message.getChannelId())
+                    .orElseThrow(() -> new NoSuchElementException("채널이 없습니다."));
+            channel.removeMessageId(messageId);
+            channelRepository.save(channel);
+        }
     }
 }
