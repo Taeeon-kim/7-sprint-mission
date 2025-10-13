7b3583f64361fad808c902489ed44a48b7dfab5f
taeeonkim <myhom43@gmail.com>
2025-10-13 15:42:21 +0900

    feature: JCF Repository를 데이터 관리를 위한 InMemoryStore 추가
    - 인메모리 데이터 store 추가 for users, channels, and messages with HashMap.

---
+++ src/main/java/com/sprint/mission/discodeit/store/InMemoryStore.java
@@ -0,0 +1,15 @@
+package com.sprint.mission.discodeit.store;
+
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.entity.User;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class InMemoryStore {
+    // NOTE: 원래 OOP 준수를 위해 캡슐화 getter, setter해야하지만 학습, 테스트 임시용을 위해 public으로 사용
+    public final HashMap<UUID, User> users = new HashMap<>();
+    public final HashMap<UUID, Channel> channels = new HashMap<>();
+    public final HashMap<UUID, Message> messages = new HashMap<>();
+}
