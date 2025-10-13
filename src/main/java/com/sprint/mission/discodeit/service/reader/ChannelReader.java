0fa607c09e34f46e43fa9fa37174fbf580b29f7d
taeeonkim <myhom43@gmail.com>
2025-10-13 23:27:36 +0900

    feature: User 와 Channel repository를 위한 helper 리더 생성
    -  userRepository 의존 읽기전용 `UserReader` 구현
    - channelRepository 의존 읽기전용 `ChannelReader` 구현

---
+++ src/main/java/com/sprint/mission/discodeit/service/reader/ChannelReader.java
@@ -0,0 +1,24 @@
+package com.sprint.mission.discodeit.service.reader;
+
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.repository.ChannelRepository;
+
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
+public class ChannelReader {
+
+    private final ChannelRepository channelRepository;
+
+    public ChannelReader(ChannelRepository channelRepository) {
+        this.channelRepository = channelRepository;
+    }
+
+    public Channel findChannelOrThrow(UUID channelId) {
+        if (channelId == null) {
+            throw new IllegalArgumentException("입력값이 잘못 되었습니다.");
+        }
+        return channelRepository.findById(channelId)
+                .orElseThrow(()-> new NoSuchElementException("채널이 없습니다."));
+    }
+}
