868d33ab36ba8110c66a3ae71e63f853a6afa47e
yumi Kim <cdcd0214@gmail.com>
2025-10-15 17:41:59 +0900

    주석 일부 변경

--- src/main/java/com/sprint/mission/discodeit/service/file/FileUserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/file/FileUserService.java
@@ -8,11 +8,11 @@ import java.util.*;
 
 public class FileUserService implements UserService {
 
-    // 파일 이름 정의
+    // 파일 저장 경로
     private static final String FILENAME
             = "D:\\codeit07\\7-sprint-mission\\src\\main\\java\\com\\sprint\\mission\\discodeit\\repository\\file\\users.sav";
 
-    // 메모리 캐시
+    // 임시저장
     private final Map<UUID, User> users = new HashMap<>();
 
     //싱글톤
