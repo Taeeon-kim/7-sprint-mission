b543e4d8d4f7134cc5f67bbea7e18216eca1832a
yumi Kim <cdcd0214@gmail.com>
2025-10-16 14:49:22 +0900

    FileMessageRepository 작성

---
+++ src/main/java/com/sprint/mission/discodeit/repository/file/FileMessageRepository.java
@@ -0,0 +1,100 @@
+package com.sprint.mission.discodeit.repository.file;
+
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+import com.sprint.mission.discodeit.service.file.FileMessageSerivce;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class FileMessageRepository implements MessageRepository {
+
+    //파일 저장 경로
+    private static final String FILENAME
+            = "D:\\codeit07\\7-sprint-mission\\src\\main\\java\\com\\sprint\\mission\\discodeit\\repository\\file\\message.sav";
+
+    // 임시저장
+    private final Map<UUID, Message> messages = new LinkedHashMap<>();
+
+    //싱글톤
+    private static final FileMessageRepository INSTANCE = new FileMessageRepository();
+
+    private FileMessageRepository(){
+        loadMessageFromFile();
+    }
+
+    public static FileMessageRepository getInstance(){
+        return INSTANCE;
+    }
+
+    // 파일 갖고 오기(역직렬화)
+    private void loadMessageFromFile(){
+        File file = new File(FILENAME);
+        if(!file.exists()){
+            return;
+        }
+
+        try(ObjectInputStream ois
+                    = new ObjectInputStream(new FileInputStream(file))){
+            Object obj = ois.readObject();
+            if(obj instanceof Map<?, ?> map){
+                for(Object key : map.keySet()){
+                    if(key instanceof UUID && map.get(key) instanceof Message m){
+                        messages.put((UUID) key, m);
+                    }
+                }
+            }
+        }catch (Exception e){
+            System.out.println("파일로드실패");
+            e.printStackTrace();
+        }
+    }
+
+    // 직렬화
+    private void saveMessageToFile(){
+        try(ObjectOutputStream oos
+                    =new ObjectOutputStream(new FileOutputStream(FILENAME))){
+            oos.writeObject(this.messages);
+        } catch (Exception e) {
+            System.out.println("저장 실패");
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void save(Message message) {
+        messages.put(message.getId(), message);
+        saveMessageToFile();
+    }
+
+    @Override
+    public Message findByMessage(UUID uuid) {
+        return messages.get(uuid);
+    }
+
+    @Override
+    public List<Message> findAll(User userId) {
+        return messages.values().stream()
+                .filter(m->m.getSendUser().equals(userId.getId())
+                || m.getReceiverUser().equals(userId.getId()))
+                .sorted(Comparator.comparingLong(Message::getCreateAt))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void updateMessage(UUID uuid, String newMessage) {
+        Message m = messages.get(uuid);
+        if( m != null){
+            m.setInputMsg(newMessage);
+            saveMessageToFile();
+        }
+    }
+
+    @Override
+    public void deleteMessage(UUID uuid) {
+        messages.remove(uuid);
+        saveMessageToFile();
+    }
+}
