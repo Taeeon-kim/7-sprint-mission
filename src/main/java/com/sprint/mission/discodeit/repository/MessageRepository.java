95a5923eb1a001fc01c225df45271ee49fb21274
yumi Kim <cdcd0214@gmail.com>
2025-10-16 11:24:39 +0900

    Repository Interface 작성

---
+++ src/main/java/com/sprint/mission/discodeit/repository/MessageRepository.java
@@ -0,0 +1,21 @@
+package com.sprint.mission.discodeit.repository;
+
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.entity.User;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface MessageRepository {
+
+    void save(Message message); // 메시지 저장
+
+    Message findByMessage(UUID uuid); // 메시지 조회
+
+    List<Message> findAll(User userId); // 유저관련 메시지 전체조회
+
+    void updateMessage(UUID uuid, String newMessage); // 메시지 수정
+
+    void deleteMessage(UUID uuid); // 메시지 삭제
+
+}
