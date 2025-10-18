7c41b5a79e87da34ed4f23a4c4191acec21f8629
yumi Kim <cdcd0214@gmail.com>
2025-10-18 16:03:59 +0900

    JCFMessageRepository 작성

--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFMessageService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFMessageService.java
@@ -2,13 +2,15 @@ package com.sprint.mission.discodeit.service.jcf;
 
 import com.sprint.mission.discodeit.entity.Message;
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.MessageRepository;
+import com.sprint.mission.discodeit.repository.jcf.JCFMessageRepository;
 import com.sprint.mission.discodeit.service.MessageService;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class JCFMessageService implements MessageService {
-    private final Map<UUID, Message> messages = new LinkedHashMap<>();
+
+    private final MessageRepository repository = JCFMessageRepository.getInstance();
 
     private JCFMessageService() {
         System.out.println("생성자 private");
@@ -22,41 +24,27 @@ public class JCFMessageService implements MessageService {
 
     @Override
     public void createMsg(Message msg) {
-        messages.put(msg.getId(), msg);
+        repository.save(msg);
         System.out.println("[DM전송] : " + msg);
     }
 
     @Override
     public Message getMsg(UUID uuid) {
-        return messages.get(uuid);
+        return repository.findByMessage(uuid);
     }
 
     @Override
     public List<Message> getAllMsg(User user) {
-        return messages.values().stream()
-                .filter(m->m.getSendUser().equals(user.getId()) || m.getReceiverUser().equals(user.getId()))
-                .sorted(Comparator.comparingLong(Message::getCreateAt))
-                .collect(Collectors.toList());
-
-//        List<Message> result = new ArrayList<>();
-//        for(Message m : messages.values()){
-//            // user가 보낸 메시지 or user가 받은 메시지 둘 다 포함
-//            if(m.getSendUser().equals(user.getId()) || m.getReceiverUser().equals(user.getId())){
-//                result.add(m);
-//            }
-//        }
-//        result.sort(Comparator.comparingLong(Message::getCreateAt));
-//        return result;
+        return repository.findAll(user);
     }
 
     @Override
     public void updateMsg(UUID uuid, String newMsg) {
-        Message m = messages.get(uuid);
-        if ( m != null) m.setInputMsg(newMsg);
+        repository.findByMessage(uuid);
     }
 
     @Override
     public void deleteMsg(UUID uuid) {
-        messages.remove(uuid);
+        repository.deleteMessage(uuid);
     }
 }
