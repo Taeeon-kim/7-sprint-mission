b2f9765f40478a5f7e76bfb9e5f2bc46671fe6e8
yumi Kim <cdcd0214@gmail.com>
2025-10-18 15:39:25 +0900

    JCFUserRepository 작성

--- src/main/java/com/sprint/mission/discodeit/service/jcf/JCFUserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/jcf/JCFUserService.java
@@ -1,12 +1,15 @@
 package com.sprint.mission.discodeit.service.jcf;
 
 import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.repository.jcf.JCFUserRepository;
 import com.sprint.mission.discodeit.service.UserService;
 
 import java.util.*;
 
 public class JCFUserService implements UserService {
-    private final Map<UUID, User> users = new HashMap<>();
+
+    private final UserRepository repository = JCFUserRepository.getInstance();
 
     private JCFUserService() {
     }
@@ -19,34 +22,32 @@ public class JCFUserService implements UserService {
 
     @Override
     public void createUser(User user) {
-        users.put(user.getId(), user);
+        repository.save(user);
         System.out.println("[User 생성] : " + user);
     }
 
     @Override
     public User readUser(UUID uuid) {
-        return users.get(uuid);
+        return repository.findById(uuid);
     }
 
     @Override
     public List<User> readAllUser() {
-        return new ArrayList<>(users.values());
+        return repository.findAll();
     }
 
     @Override
     public void updateUser(UUID uuid, String newName) {
-        User u = users.get(uuid);
-        if( u != null) u.setNickName(newName);
+        repository.updateNickName(uuid, newName);
     }
 
     @Override
     public void updatePw(UUID uuid, String newPassword) {
-        User u = users.get(uuid);
-        if( u != null ) u.setUserPassword(newPassword);
+        repository.updatePassword(uuid, newPassword);
     }
 
     @Override
     public void deleteUser(UUID uuid) {
-        users.remove(uuid);
+        repository.delete(uuid);
     }
 }
