7a672d5f883661e10a7003d885d6b7ca92b1e69d
taeeonkim <myhom43@gmail.com>
2025-10-23 10:03:02 +0900

    feat: 엔티티, 서비스, 리포지토리 sprint context 빈 주입 및 주요 서비스 로직 초기화 코드 추가
    - spring context로 서비스 인스턴스 주입 및 활용.
    - User, Channel, Message 엔티티 및 연관 서비스 로직 초기화.
    - User, Channel, Message CRUD 및 관련 메서드 호출 테스트 코드 추가.
    - 서비스 간 동작 확인용 샘플 데이터와 출력 로직 작성.

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -1,13 +1,168 @@
 package com.sprint.mission.discodeit;
 
+import com.sprint.mission.discodeit.entity.Channel;
+import com.sprint.mission.discodeit.entity.Message;
+import com.sprint.mission.discodeit.entity.User;
+
+import com.sprint.mission.discodeit.service.ChannelService;
+import com.sprint.mission.discodeit.service.MessageService;
+import com.sprint.mission.discodeit.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.List;
+import java.util.UUID;
 
 @SpringBootApplication
 public class DiscodeitApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiscodeitApplication.class, args);
+
+        ConfigurableApplicationContext context = SpringApplication.run(DiscodeitApplication.class, args);
+        // 서비스 초기화
+        // TODO context에서 Bean을 조회하여 각 서비스 구현체 할당 코드 작성하세요.
+
+
+        final UserService userService  = context.getBean(UserService.class);
+        final ChannelService channelService = context.getBean(ChannelService.class);
+        final MessageService messageService = context.getBean(MessageService.class);
+
+        List<User> allUsers = userService.getAllUsers();
+        System.out.println("allUsers = " + allUsers);
+        // ---- 유저 만들기 (회원가입) ------
+        userService.signUp("youngble", "email@example.com", "password123", "010-1234-5678");
+        userService.signUp("zzzz", "zzzz@example.com", "zzzz1123", "010-3334-44444");
+        userService.signUp("Hong", "test@example.com", "wssss123123", "010-1122-3333");
+        System.out.println("=============================== ");
+        allUsers = userService.getAllUsers();
+        System.out.println("allUsers = " + allUsers);
+        System.out.println("=============================== ");
+
+        // ---- 유저 지우기 (회원 탈퇴) ------
+        userService.deleteUser(allUsers.get(0).getId());
+//        userService.deleteUser(allUsers.get(0).getId());
+        allUsers = userService.getAllUsers();
+        System.out.println("allUsers = " + allUsers);
+
+        System.out.println("===========update user==================== ");
+        //--- 유저 수정하기(정보 업데이트) -------
+        // ----- 클라이언트 요청 api 데이터, 실제 dto 형식으로 받아서 dto.nickname 같은식으로 넘겨줌 ---
+        userService.updateUser(allUsers.get(0).getId(), "changedName!!", null, "changedPassword!!", "");
+//        User userById = userService.getUserById(user2.getId());
+        allUsers = userService.getAllUsers();
+        System.out.println("allUsers = " + allUsers);
+
+        // ------- 채널 조회 ----
+        List<Channel> allChannels = channelService.getAllChannels();
+        System.out.println("allChannels = " + allChannels);
+
+        System.out.println("=============================== ");
+
+        // ------- 채널 만들기  ----
+
+        channelService.createChannel("첫 채널 타이틀", "첫 채널입니다 마음껏 메세지를 주고받으세요", allUsers.get(0).getId());
+        channelService.createChannel("두번째 타이틀", "두번째 채널 공지 채널입니다.", allUsers.get(1).getId());
+        allChannels = channelService.getAllChannels();
+        System.out.println("all channels after creating channel :" + allChannels);
+
+        System.out.println("=============================== ");
+
+        channelService.updateChannel(allChannels.get(0).getId(), "changed title", "changed description");
+        allChannels = channelService.getAllChannels();
+        System.out.println("all channels after updating channel :" + allChannels);
+        System.out.println("=============================== ");
+
+        channelService.updateChannel(allChannels.get(0).getId(), null, "changed description");
+        allChannels = channelService.getAllChannels();
+        System.out.println("all channels after updating channel2 :" + allChannels);
+        System.out.println("=============================== ");
+
+
+        // --- 채널 맴버 조회 ----
+        List<User> allMembers = channelService.getAllMembers(allChannels.get(0).getId());
+        System.out.println("all members of channel before join channel: " + allMembers);
+
+        System.out.println("=============================== ");
+
+        // --- 채널 조인 ----
+        UUID chId = allChannels.get(0).getId();
+        allUsers
+                .forEach(member -> {
+                    channelService.joinChannel(chId, member.getId());
+                });
+
+        allMembers = channelService.getAllMembers(chId);
+        System.out.println("all members of channel after join channel: " + allMembers);
+
+        System.out.println("=============================== ");
+
+        System.out.println("user's channel: " + channelService.getChannelsByUserId(allUsers.get(0).getId()));
+        System.out.println("=============================== ");
+        // -- 채널 나가기 ----
+        channelService.leaveChannel(chId, allMembers.get(0).getId());
+
+        allMembers = channelService.getAllMembers(chId);
+        System.out.println("all members of channel after leave channel: " + allMembers);
+        System.out.println("=============================== ");
+        System.out.println("user's channel after leave: " + channelService.getChannelsByUserId(allUsers.get(0).getId()));
+
+        System.out.println("=============================== ");
+
+        // --- 채널의 모든 메세지 확인 ---
+        List<Message> allMessagesOfChannel = messageService.getAllMessagesOfChannel(chId);
+        System.out.println("chId 1 = " + chId);
+        System.out.println("all messages of channel before send message: " + allMessagesOfChannel);
+
+        System.out.println("=============================== ");
+
+        // --- 메세지 보내기 --
+
+        messageService.sendMessageToChannel(chId, allMembers.get(0).getId(), "첫번째 message메세지입니다");
+        allMessagesOfChannel = messageService.getAllMessagesOfChannel(chId);
+        System.out.println("chId 2 = " + chId);
+        System.out.println("all messages of channel after send message: " + allMessagesOfChannel);
+
+        System.out.println("=============================== ");
+        System.out.println("user's channel: " + channelService.getChannelsByUserId(allMembers.get(0).getId()));
+        System.out.println("=============================== ");
+
+
+        // -- 모든 메세지 보기 ---
+        List<Message> allMessages = messageService.getAllMessages();
+        System.out.println("allMessages = " + allMessages);
+        System.out.println("=============================== ");
+        messageService.sendMessageToChannel(chId, allMembers.get(0).getId(), "두번째 message메세지입니다");
+        messageService.sendMessageToChannel(chId, allMembers.get(0).getId(), "세번째 message메세지입니다");
+        allMessages = messageService.getAllMessages();
+        System.out.println("allMessages after send Message = " + allMessages);
+        System.out.println("=============================== ");
+
+        // -- 단일 특정 메세지 보기 --
+        Message messageById = messageService.getMessageById(allMessages.get(0).getId());
+        System.out.println("messageById = " + messageById);
+        System.out.println("=============================== ");
+
+        // -- 메세지 업데이트하기 --
+        messageService.updateMessage(messageById.getId(), "updated message!!");
+        messageById = messageService.getMessageById(messageById.getId());
+        System.out.println("updated messageById = " + messageById);
+        allMessagesOfChannel = messageService.getAllMessagesOfChannel(chId);
+        System.out.println("allMessagesOfChannel = " + allMessagesOfChannel);
+        // -- 메세지 제거하기 --
+        messageService.deleteMessage(messageById.getId());
+        allMessages = messageService.getAllMessages();
+        System.out.println("allMessages after delecting a message= " + allMessages);
+        allMessagesOfChannel = messageService.getAllMessagesOfChannel(chId);
+        System.out.println("allMessagesOfChannel after delecting a message= " + allMessagesOfChannel);
+        // -- 채널 제거하기 ---
+        channelService.deleteChannel(allChannels.get(0).getId());
+        allChannels = channelService.getAllChannels();
+        System.out.println("all channels after deleting the channel :" + allChannels);
+        allMessages = messageService.getAllMessages();
+        System.out.println("allMessages after delecting the channel = " + allMessages);
+
+
 	}
 
 }
