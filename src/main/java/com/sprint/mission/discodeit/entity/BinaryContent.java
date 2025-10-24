376b06da811dc3126f49691b7ff5979141d22839
yumi Kim <cdcd0214@gmail.com>
2025-10-24 11:53:54 +0900

    BinaryContent, ReadStatus, UserStatus 의 Domain Entity 작성

---
+++ src/main/java/com/sprint/mission/discodeit/entity/BinaryContent.java
@@ -0,0 +1,35 @@
+package com.sprint.mission.discodeit.entity;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.Instant;
+import java.util.UUID;
+
+/**
+ * 이미지, 파일 등 바이너리 데이터를 표현하는 도메인 모델
+ * 사용자의 프로필 이미지, 메시지에 첨부된 파일을 저장하기 위해 활용
+ * 수정 불가능한 도메인 모델로 간주함, 따라서 updateAt필드는 정의하지 않음
+ * User, Message 도메인 모델과의 의존 관계 방향성을 고려해 id 참조 필드 추가
+ */
+@Getter @ToString
+public class BinaryContent {
+
+    private final UUID uuid;
+    private final UUID userId;
+    private final UUID messageId;
+    private final byte[] data;
+    private final String fileName;
+    private final String contentType;
+    private final Instant createdAt;
+
+    public BinaryContent(UUID uuid, UUID userId, UUID messageId, byte[] data, String fileName, String contentType, Instant createdAt) {
+        this.uuid = uuid;
+        this.userId = userId;
+        this.messageId = messageId;
+        this.data = data;
+        this.fileName = fileName;
+        this.contentType = contentType;
+        this.createdAt = createdAt;
+    }
+}
---
+++ src/main/java/com/sprint/mission/discodeit/entity/UserStatus.java
@@ -0,0 +1,35 @@
+package com.sprint.mission.discodeit.entity;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.UUID;
+
+/**
+ * 사용자 별 마지막으로 확인된 접속 시간을 표현하는 도메인 모델
+ * 사용자의 온라인 상태를 확인하기 위해 활용
+ * 마지막 접속 시간을 기준으로 현재 로그인한 유저로 판단할 수 있는 메소드 정의
+ * - 마지막 접속 시간이 현재 시간으로부터 5분 이내이면 현재 접속중인 유저로 간주함
+ */
+@Getter @ToString
+public class UserStatus{
+
+    private final UUID userId; // 접속 유저
+    private Instant lastActiveAt; // 마지막 접속 시간
+
+    public UserStatus(UUID userId, Instant lastActiveAt) {
+        this.userId = userId;
+        this.lastActiveAt = lastActiveAt;
+    }
+
+    public void updateLastActiveAt(Instant time) {
+        this.lastActiveAt = time;
+    }
+
+    public boolean isOnline() {
+        return Duration.between(lastActiveAt, Instant.now()).toMinutes() <= 5;
+    }
+
+}
