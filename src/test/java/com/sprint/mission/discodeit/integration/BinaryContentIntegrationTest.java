d6c6b66c1da269e361651b3a6b5e50389ca33ccb
taeeonkim <myhom43@gmail.com>
2025-10-29 15:25:14 +0900

    test: BinaryContent 업로드 테스트에 필수 필드 검증 및 예외 처리 로직 추가
    - 테스트 클래스에 @DisplayName 추가
    - 필수 필드 충족 확인 및 저장 로직 테스트 반영
    - BinaryContent 조회 로직에서 orElseThrow 적용
    - TODO 주석으로 실제 이미지파일 기반 테스트 추가 계획 명시

--- src/test/java/com/sprint/mission/discodeit/integration/BinaryContentIntegrationTest.java
+++ src/test/java/com/sprint/mission/discodeit/integration/BinaryContentIntegrationTest.java
@@ -10,6 +10,7 @@ import com.sprint.mission.discodeit.service.basic.BasicBinaryContentService;
 import com.sprint.mission.discodeit.store.InMemoryStore;
 import org.junit.jupiter.api.*;
 
+import java.util.NoSuchElementException;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -36,7 +37,7 @@ public class BinaryContentIntegrationTest {
     class Upload {
 
         @Test
-        @DisplayName("[Integration][Flow] 파일 업로드 - 필수 필드 충족, 업로드 후 id 반환 및 저장 확인")
+        @DisplayName("[Integration][Flow][Positive] 파일 업로드 - 필수 필드 충족, 업로드 후 id 반환 및 저장 확인")
         void uploadBinaryContent_then_returns_id_and_persists() {
             //given
             // TODO: 지금은 예시로 직접 텍스트값으로 fileName, contentType, bytes[] 값을 넣어줬는데 시간이 난다면 실제 이미지파일 넣는걸로 테스트 해볼것
@@ -54,6 +55,38 @@ public class BinaryContentIntegrationTest {
             assertEquals(command.getContentType(), binaryContent.getContentType());
             assertArrayEquals(command.getBytes(), binaryContent.getBytes());
 
+        }
+    }
+
+    @Nested
+    @DisplayName("getBinaryContent")
+    class getBinaryContent {
+        @Test
+        @DisplayName("[Integration][Flow][Positive] 파일 조회 - 요청 id에 해당하는 binaryContent 반환")
+        void getBinaryContent_returns_saved_content() {
+            //given
+            BinaryContent updateContent = new BinaryContent("test.png", "image/png", "test".getBytes());
+            BinaryContent saved = binaryContentRepository.save(updateContent);
+
+            // when
+            BinaryContent binaryContent = binaryContentService.getBinaryContent(saved.getId());
+
+            // then
+            assertEquals(saved.getId(), binaryContent.getId());
+            assertEquals(saved.getFileName(), binaryContent.getFileName());
+            assertEquals(saved.getContentType(), binaryContent.getContentType());
+
+            assertArrayEquals(saved.getBytes(), binaryContent.getBytes()); // 내용비교를 위해 순회하면 하나씩 비교, 원소 수, 순서, 값 모두 같으면 통과
+        }
+
+        @Test
+        @DisplayName("[Integration][Flow][Negative] 파일 조회 - 존재하지 않는 Id로 조회시 NoSuchElementException 예외")
+        void getBinaryContent_thorws_when_not_found() {
+            // given
+            UUID id = UUID.randomUUID();
+
+            // when & then
+            assertThrows(NoSuchElementException.class, () -> binaryContentService.getBinaryContent(id));
         }
     }
 }
