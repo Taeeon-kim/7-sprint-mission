74b6fa392551d1494af59d0e5c0605b99b208680
yumi Kim <cdcd0214@gmail.com>
2025-10-30 13:24:34 +0900

    ReadStatusRepository 수정, 채널 읽음 상태 관련 추가

--- src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
+++ src/main/java/com/sprint/mission/discodeit/repository/ReadStatusRepository.java
@@ -13,13 +13,18 @@ public interface ReadStatusRepository {
     // 특정 유저가 특정 채널에서 마지막으로 읽은 시점을 가져옴
     ReadStatus findByUserAndChannel(UUID userId, UUID channelId);
 
+    // 채널 참여자별 미확인 메시지 수 계산
+    List<ReadStatus> findByChannelId(UUID channelId);
+
     // 유저가 여러 채널을 어디까지 읽었는지 목록 조회
     List<ReadStatus> findByUserId(UUID userId);
 
-    // 사용자가 채널을 스크롤하거나 채널을 열었을 때 호출.
-    void updateLastAt(UUID userId, UUID channelId);
+    // 읽음 상태 업데이트
+    void update(ReadStatus readStatus);
 
     // 사용자가 채널을 떠난 경우 삭제
     void delete(UUID uuid);
 
+    // 특정 채널 읽음 상태 삭제, 채널 삭제 시 cascade
+    void deleteAllByChannelId(UUID channelId);
 }
