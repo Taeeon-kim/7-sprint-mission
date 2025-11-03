a191f7fa664db3ace884bc52a198460324b945f0
taeeonkim <myhom43@gmail.com>
2025-11-03 15:41:50 +0900

    feature: 다양한 Controller 클래스 추가
    - AuthController, BinaryContentController, ReadStatusController, UserController, MessageController 및 ChannelController 클래스 생성
    - 각 Controller에 기본적인 @RequestMapping URI 설정 추가

---
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -0,0 +1,9 @@
+package com.sprint.mission.discodeit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+}
