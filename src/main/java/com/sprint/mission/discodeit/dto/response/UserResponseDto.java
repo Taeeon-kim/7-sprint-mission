396e712d4c54e52068bcae90f7c0070d728d0f41
yumi Kim <cdcd0214@gmail.com>
2025-11-05 11:30:09 +0900

    User와 Profile 수정 때 Profile의 파일명을 띄우기 위한 코드작성

--- src/main/java/com/sprint/mission/discodeit/dto/response/UserResponseDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/response/UserResponseDto.java
@@ -1,31 +1,29 @@
 package com.sprint.mission.discodeit.dto.response;
 
-import com.sprint.mission.discodeit.entity.StatusType;
+import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.entity.UserStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
-
-import java.time.Instant;
-import java.util.UUID;
 
 @Getter
 @AllArgsConstructor
 @Builder
-public class UserReponseDto {
+public class UserResponseDto {
     private String userId;
     private String userName;
     private String userEmail;
-    private UUID profileImagePath;
+    private String userStatus;
+    private String profileImagePath;
 
-    public static UserReponseDto from(User user) {
-        return UserReponseDto.builder()
+    public static UserResponseDto from(User user, UserStatus userStatus, BinaryContent binaryContent) {
+        return UserResponseDto.builder()
                 .userId(user.getUserId())
                 .userName(user.getUserName())
                 .userEmail(user.getEmail())
-                .profileImagePath(user.getProfileImageId())
+                .userStatus(userStatus != null ? userStatus.getStatus().name() : null)
+                .profileImagePath(binaryContent.getFileName())
                 .build();
     }
 }
