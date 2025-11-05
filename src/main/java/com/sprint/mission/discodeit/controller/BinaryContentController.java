29f3d660e047c463b3b76f24402c998bdccaa592
taeeonkim <myhom43@gmail.com>
2025-11-05 13:44:12 +0900

    refactor: BinaryContentController에 파일 업로드 엔드포인트 추가 및 기존 메서드 리팩터링
    - 파일 업로드용 POST `/binary-contents` 엔드포인트 추가
    - `getAllBinaryContentsByIds` 메서드의 매핑 및 요청 방식 명시
    - MultipartFile 기반 파일 업로드 처리 로직 구현
    - 관련 임포트 및 불필요한 코드 정리

--- src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -1,14 +1,13 @@
 package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.dto.binaryContent.BinaryContentResponseDto;
-import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.dto.binaryContent.BinaryContentUploadCommand;
 import com.sprint.mission.discodeit.service.BinaryContentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.UUID;
@@ -28,12 +27,23 @@ public class BinaryContentController {
         return ResponseEntity.ok(binaryContent);
     }
 
-    @RequestMapping("/binary-contents")
+    @RequestMapping(value = "/binary-contents", method = RequestMethod.GET)
+    @ResponseBody
     public ResponseEntity<List<BinaryContentResponseDto>> getAllBinaryContentsByIds(
             @RequestParam List<UUID> ids
-    ){
+    ) {
         List<BinaryContentResponseDto> binaryContentsByIds = binaryContentService.getBinaryContentsByIds(ids);
         return ResponseEntity.ok(binaryContentsByIds);
+    }
+
+    @RequestMapping(value = "/binary-contents", method = RequestMethod.POST, consumes = "multipart/form-data")
+    @ResponseBody
+    public ResponseEntity<UUID> createBinaryContent(
+            @RequestBody MultipartFile file
+    ) {
+        BinaryContentUploadCommand fileCommand = BinaryContentUploadCommand.from(file);
+        UUID binaryId = binaryContentService.uploadBinaryContent(fileCommand);
+        return ResponseEntity.ok(binaryId);
     }
 
 }
