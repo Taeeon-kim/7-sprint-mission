c0f96a27b57821a169ef5fc8c5cb6dd1bba3937d
taeeonkim <myhom43@gmail.com>
2025-11-05 10:48:55 +0900

    refactor: BinaryContent 컨트롤러 통합 및 DTO 리팩터링
    - `BinaryContentController` 생성 및 엔드포인트 통합
    - `/api` 기반 엔드포인트로 수정
    - `BinaryContentResponseDto` 클래스 추가 및 반환 로직 리팩터링
    - 서비스 계층 반환 타입을 `BinaryContentResponseDto`로 변경
    - 불필요한 코드 제거 및 관련 로직 정리

--- src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -1,9 +1,39 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.dto.binaryContent.BinaryContentResponseDto;
+import com.sprint.mission.discodeit.entity.BinaryContent;
+import com.sprint.mission.discodeit.service.BinaryContentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.UUID;
 
 @Controller
-@RequestMapping("/binary-content")
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class BinaryContentController {
+
+    private final BinaryContentService binaryContentService;
+
+    @RequestMapping("/binary-contents/{id}")
+    public ResponseEntity<BinaryContentResponseDto> getBinaryContent(
+            @PathVariable UUID id
+    ) {
+        BinaryContentResponseDto binaryContent = binaryContentService.getBinaryContent(id);
+        return ResponseEntity.ok(binaryContent);
+    }
+
+    @RequestMapping("/binary-contents")
+    public ResponseEntity<List<BinaryContentResponseDto>> getAllBinaryContentsByIds(
+            @RequestParam List<UUID> ids
+    ){
+        List<BinaryContentResponseDto> binaryContentsByIds = binaryContentService.getBinaryContentsByIds(ids);
+        return ResponseEntity.ok(binaryContentsByIds);
+    }
+
 }
