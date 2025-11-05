ea84f54176d1619c7c80a37318732cbca40fe702
yumi Kim <cdcd0214@gmail.com>
2025-11-05 11:42:33 +0900

    AuthService구현

--- src/main/java/com/sprint/mission/discodeit/dto/response/LoginResponseDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/response/LoginResponseDto.java
@@ -1,14 +1,24 @@
 package com.sprint.mission.discodeit.dto.response;
 
 import com.sprint.mission.discodeit.entity.StatusType;
+import com.sprint.mission.discodeit.entity.User;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Builder
 public class LoginResponseDto {
     private final String userId;
-    private final String nickname;
-    private final String email;
-    private StatusType userStatus;
+    private final String userName;
+    private final String userEmail;
+
+    public static LoginResponseDto from(User user) {
+        return LoginResponseDto.builder()
+                .userId(user.getUserId())
+                .userName(user.getUserName())
+                .userEmail(user.getEmail())
+                .build();
+    }
 }
