9abbaaebc565b0db6a8e51f47e4414b2ad385004
yumi Kim <cdcd0214@gmail.com>
2025-11-24 18:53:01 +0900

    BinaryContent API 수정

--- src/main/java/com/sprint/mission/discodeit/dto/response/BinaryContentResponseDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/response/BinaryContentResponseDto.java
@@ -4,21 +4,30 @@ import com.sprint.mission.discodeit.entity.BinaryContent;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.Instant;
 import java.util.UUID;
 
 @Getter
 @Builder
 public class BinaryContentResponseDto {
 
-    private UUID messageId;
-    private String fileName;
-    private long size;
+//    private UUID messageId;
+    private UUID id;
+//    private String fileName;
+    private String contentType;
+//    private long size;
+    private long bytes;
+    private Instant createdAt;
 
     public static BinaryContentResponseDto from(BinaryContent binaryContent) {
         return BinaryContentResponseDto.builder()
-                .messageId(binaryContent.getUuid())
-                .fileName(binaryContent.getFileName())
-                .size(binaryContent.getBytes().length)
+//                .messageId(binaryContent.getUuid())
+                .id(binaryContent.getUuid())
+                .contentType(binaryContent.getContentType())
+//                .fileName(binaryContent.getFileName())
+//                .size(binaryContent.getBytes().length)
+                .bytes(binaryContent.getBytes().length)
+                .createdAt(binaryContent.getCreateAt())
                 .build();
     }
 }
