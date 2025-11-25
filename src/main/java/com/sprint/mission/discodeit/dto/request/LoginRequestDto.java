811c718b23dc447fe2ed2607679c5df64497029d
yumi Kim <cdcd0214@gmail.com>
2025-11-25 15:43:46 +0900

    Auth API HttpStatus 수정

--- src/main/java/com/sprint/mission/discodeit/dto/request/LoginRequestDto.java
+++ src/main/java/com/sprint/mission/discodeit/dto/request/LoginRequestDto.java
@@ -1,10 +1,12 @@
 package com.sprint.mission.discodeit.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Schema(description = "로그인 정보")
 public class LoginRequestDto {
     private final String username;
     private final String password;
--- src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/discodeit/exception/GlobalExceptionHandler.java
@@ -3,16 +3,31 @@ package com.sprint.mission.discodeit.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(IllegalArgumentException e) {
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(e.getMessage());
+    // 400 Bad Request 반환
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleIllegalArgument(IllegalArgumentException e) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", e.getMessage());
+        return response;
     }
 
+    // 404 Not Fount 반환
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND) // 404
+    public Map<String, String> handleNotFound(NoSuchElementException e) {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", e.getMessage());
+        return response;
+    }
 }
