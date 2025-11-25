5fb8a3fbb75966a9e6ff08cc3cfc1c8a044349c5
yumi Kim <cdcd0214@gmail.com>
2025-11-25 17:41:39 +0900

    Message API HttpStatus 수정

--- src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/MessageController.java
@@ -5,6 +5,10 @@ import com.sprint.mission.discodeit.dto.request.MessageUpdateRequestDto;
 import com.sprint.mission.discodeit.dto.response.MessageResponseDto;
 import com.sprint.mission.discodeit.repository.ChannelRepository;
 import com.sprint.mission.discodeit.service.MessageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -17,13 +21,18 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/messages")
 @RequiredArgsConstructor
+@Tag(name = "Message")
 public class MessageController {
     private final MessageService messageService;
     private final ChannelRepository channelRepository;
 
     // 메시지 전송(저장)
-//    @RequestMapping(method = RequestMethod.POST, consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     @PostMapping(consumes =  MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "Message 생성")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Message가 성공적으로 생성됨"),
+            @ApiResponse(responseCode = "404", description = "Channel 또는 User를 찾을 수 없음")
+    })
     public MessageResponseDto createMessage(@ModelAttribute MessageCreateRequestDto messageCreateRequestDto,
                                             @RequestPart(value = "file", required = false) List<MultipartFile> files) {
         System.out.println("files: " + files);
@@ -32,8 +41,13 @@ public class MessageController {
     }
 
     // 메시지 수정
-//    @RequestMapping(method = RequestMethod.PUT, consumes = "multipart/form-data")
     @PatchMapping(value = "/{messageId}", consumes = "multipart/form-data")
+    @Operation(summary = "Message 내용 수정")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Message가 성공적으로 수정됨"),
+            @ApiResponse(responseCode = "400", description = "Message"),
+            @ApiResponse(responseCode = "404", description = "Message를 찾을 수 없음")
+    })
     public MessageResponseDto updateMessage(
             @ModelAttribute("data") MessageUpdateRequestDto messageUpdateRequestDto,
             @RequestPart(value = "file", required = false) List<MultipartFile> files) {
@@ -42,15 +56,22 @@ public class MessageController {
     }
 
     // 메시지 삭제
-//    @RequestMapping(method = RequestMethod.DELETE, params = "uuid")
     @DeleteMapping("/{messageId}")
+    @Operation(summary = "Message 삭제")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Message가 성공적으로 삭제됨"),
+            @ApiResponse(responseCode = "404", description = "Message를 찾을 수 없음")
+    })
     public void deleteMessage(@PathVariable UUID uuid) {
         messageService.deleteMessage(uuid);
     }
 
     // 특정 채널 메시지 목록 조회
-//    @RequestMapping(method = RequestMethod.GET, params = "channelId")
     @GetMapping
+    @Operation(summary = "Channel의 Message 목록 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Message 목록 조회 성공")
+    })
     public List<MessageResponseDto> getMessageByChannel(@RequestParam UUID channelId) {
         var channel = channelRepository.findByChannel(channelId)
                 .orElseThrow(() -> new IllegalArgumentException("채널을 찾을 수 없습니다."));
