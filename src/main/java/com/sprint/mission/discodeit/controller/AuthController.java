7d5034708b4593e9bcfe923801d035e1a1a13658
yumi Kim <cdcd0214@gmail.com>
2025-11-27 14:37:30 +0900

    AuthApi Interface 분리

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.controller.api.AuthApi;
 import com.sprint.mission.discodeit.dto.request.LoginRequestDto;
 import com.sprint.mission.discodeit.dto.response.LoginResponseDto;
 import com.sprint.mission.discodeit.service.AuthService;
@@ -17,17 +18,9 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 @Tag(name = "Auth")
-public class AuthController {
+public class AuthController implements AuthApi {
     private final AuthService authService;
 
-
-    @PostMapping("/login")
-    @Operation(summary = "로그인")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "로그인 성공"),
-            @ApiResponse(responseCode = "400", description = "비밀번호가 일치하지 않음"),
-            @ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없음")
-    })
     public ResponseEntity<LoginResponseDto> login(@RequestBody LoginRequestDto loginRequestDto) {
         return ResponseEntity.ok(authService.login(loginRequestDto));
     }
