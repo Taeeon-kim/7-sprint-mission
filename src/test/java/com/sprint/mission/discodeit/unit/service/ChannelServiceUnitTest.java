df44d16804ce5a8e69d5c2ff36cec506ab07b0fe
taeeonkim <myhom43@gmail.com>
2025-12-18 23:30:22 +0900

    test: 채널 수정, 삭제 및 예외 처리 관련 단위 테스트 추가
    - 채널 수정 테스트 케이스 작성 (public/private 구분 및 예외 처리 검증)
    - 채널 삭제 테스트 케이스 추가 및 요청 데이터 유효성 검증 로직 반영
    - 새로운 예외 처리 로직 테스트 (`ChannelModificationNotAllowedException`, `ChannelInvalidParticipantsException`)
    - 코드 가독성 및 테스트 구조 개선 (중복 제거 및 리팩토링)

--- src/test/java/com/sprint/mission/discodeit/unit/service/ChannelServiceUnitTest.java
+++ src/test/java/com/sprint/mission/discodeit/unit/service/ChannelServiceUnitTest.java
@@ -1,12 +1,14 @@
 package com.sprint.mission.discodeit.unit.service;
 
-import com.sprint.mission.discodeit.dto.channel.ChannelCreateCommand;
-import com.sprint.mission.discodeit.dto.channel.ChannelCreateRequestDto;
-import com.sprint.mission.discodeit.dto.channel.ChannelResponseDto;
+import com.sprint.mission.discodeit.dto.channel.*;
 import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.entity.type.ChannelType;
+import com.sprint.mission.discodeit.exception.DiscodeitException;
+import com.sprint.mission.discodeit.exception.ErrorCode;
+import com.sprint.mission.discodeit.exception.channel.ChannelInvalidParticipantsException;
 import com.sprint.mission.discodeit.exception.channel.ChannelMinimumMembersNotMetException;
+import com.sprint.mission.discodeit.exception.channel.ChannelModificationNotAllowedException;
 import com.sprint.mission.discodeit.mapper.ChannelMapper;
 import com.sprint.mission.discodeit.repository.ChannelRepository;
 import com.sprint.mission.discodeit.repository.MessageRepository;
@@ -23,12 +25,13 @@ import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
@@ -161,6 +164,122 @@ public class ChannelServiceUnitTest {
             then(readStatusRepository).shouldHaveNoInteractions();
         }
 
+        @DisplayName("[Negative] 채널 참가 인원에 잘못된 인원이있어 요청 인원보다 작을시 ChannelInvalidParticipantsException 예외발생")
+        @Test
+        void createPrivateChannel_shouldThrow_whenUserIdsLessThenRequest() {
+            // given
+            List<UUID> memberIds = List.of(UUID.randomUUID(), UUID.randomUUID(), UUID.randomUUID());
+            ChannelCreateRequestDto dto = new ChannelCreateRequestDto(null, null, memberIds);
+            ChannelCreateCommand command = ChannelCreateCommand.from(dto, ChannelType.PRIVATE);
+
+            List<User> users = List.of(mock(User.class), mock(User.class));
+            Channel privateChannel = mock(Channel.class);
+
+            given(channelFactory.create(command)).willReturn(privateChannel);
+            given(channelRepository.save(privateChannel)).willReturn(privateChannel);
+            given(privateChannel.getType()).willReturn(ChannelType.PRIVATE);
+            given(userReader.findUsersByIds(command.memberIds())).willReturn(users);
+
+            //when + then
+            assertThrows(ChannelInvalidParticipantsException.class, () -> channelService.createChannel(command));
+
+            then(channelFactory).should().create(command);
+            then(channelRepository).should().save(privateChannel);
+            then(userReader).should().findUsersByIds(command.memberIds());
+
+            then(readStatusRepository).shouldHaveNoInteractions();
+            then(channelMapper).shouldHaveNoInteractions();
+
+        }
+
+    }
+
+    @Nested
+    @DisplayName("채널 수정 - public, private")
+    class updateChannel {
+        @Test
+        @DisplayName("[Behavior][Positive] 채널 수정 - PUBLIC 채널 update, channelRepository.save() 정상 위임")
+        void updateChannel_shouldUpdateAndSave_whenPublicChannel() {
+
+            // given
+            UUID channelId = UUID.randomUUID();
+            ChannelUpdateRequestDto request = new ChannelUpdateRequestDto("newTitme", "newDesc");
+
+            Channel channel = mock(Channel.class);
+            given(channelRepository.findById(channelId)).willReturn(Optional.of(channel));
+            given(channel.getType()).willReturn(ChannelType.PUBLIC);
+
+            // when
+            channelService.updateChannel(channelId, request);
+
+            // then
+            then(channel).should().update(any(ChannelUpdateParams.class));
+
+            then(channelRepository).should().save(channel);
+        }
+
+        @Test
+        @DisplayName("[Behavior + Branch][Negative] 채널 수정 - PRIVATE 채널이면 수정 불가 ChannelModificationNotAllowedException 예외발생 update 미호출, save 미위임")
+        void updateChannel_shouldThrow_whenPrivateChannel() {
+            // given
+            UUID channelId = UUID.randomUUID();
+            ChannelUpdateRequestDto request = new ChannelUpdateRequestDto("newTitme", "newDesc");
+            Channel channel = mock(Channel.class);
+            given(channelRepository.findById(channelId)).willReturn(Optional.of(channel));
+            given(channel.getType()).willReturn(ChannelType.PRIVATE);
+
+            // when & then
+            assertThrows(ChannelModificationNotAllowedException.class, () -> channelService.updateChannel(channelId, request));
+            then(channel).should(never()).update(any(ChannelUpdateParams.class));
+            then(channelRepository).should(never()).save(any(Channel.class));
+
+        }
+
+        @Test
+        @DisplayName("[Branch + Exception][Negative] 채널 수정 - channelId이 null 이면 INVALID_INPUT 예외 전파")
+        void updateChannel_shouldThrow_whenChannelIdIsNull() {
+            // given
+            UUID channelId = null;
+            ChannelUpdateRequestDto request = new ChannelUpdateRequestDto("newTitme", "newDesc");
+
+            // when & then
+            DiscodeitException ex = assertThrows(DiscodeitException.class, () -> channelService.updateChannel(channelId, request));
+
+            assertEquals(ErrorCode.INVALID_INPUT, ex.getErrorCode());
+
+            then(channelRepository).shouldHaveNoInteractions();
+        }
+    }
+
+    @Nested
+    @DisplayName("채널 삭제")
+    class deleteChannel{
+        @Test
+        @DisplayName("[Behavior][Positive] 채널 삭제 - message, readStats, channel 삭제 위임")
+        void deleteChannel_shouldDeleteChannelAndReadStatusAndMessage(){
+            // given
+            UUID channelId = UUID.randomUUID();
+            Channel channel = mock(Channel.class);
+            given(channelReader.findChannelOrThrow(channelId)).willReturn(channel);
+            given(channel.getId()).willReturn(channelId);
+
+            // when
+            channelService.deleteChannel(channelId);
+
+            // then
+            then(messageRepository).should().deleteByChannelId(channel.getId());
+            then(readStatusRepository).should().deleteByChannelId(channel.getId());
+            then(channelRepository).should().deleteById(channel.getId());
+        }
+
+        @Test
+        @DisplayName("[Branch][Negative] 채널 삭제 - channelId가 null일시 INVALID_INPUT 전파")
+        void deleteChannel_shouldThrow_whenChannelIdIsNull(){
+
+            // when & then
+            DiscodeitException ex = assertThrows(DiscodeitException.class, () -> channelService.deleteChannel(null));
+            assertEquals(ErrorCode.INVALID_INPUT, ex.getErrorCode());
+        }
     }
 
 }
